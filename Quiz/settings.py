10da2ab939f2f6613b2ebc1c2a13c4b8e5434ac3
cybermizdav26 <cybermizrobov72022@gmail.com>
2024-03-06 18:58:58 +0500

    a commit

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u^uk%a%h5=g!kf^)p7sok1u^&ofq=y#+f3qxlune63)p)okyq=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://quiztests.pythonanywhere.com/']
+ALLOWED_HOSTS = ['QuizTests.pythonanywhere.com']
 
 
 # Application definition
