c7d7e057ff85b3926c1f416e74b6e5a3aa01ba96
cybermizdav26 <cybermizrobov72022@gmail.com>
2024-03-15 19:35:48 +0500

    css style

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -125,9 +125,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = 'static/'
 
-# MEDIA_URL = 'media/'
-#
-# MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
